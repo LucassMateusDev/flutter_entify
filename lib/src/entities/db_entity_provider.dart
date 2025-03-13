1f349ea144aa5b171841e6dabb01c8ac23904410
LucassMateus <spechtlucasmateus@gmail.com>
2025-03-13 13:47:47 -0300

    refactor: remove unused DbEntityProvider, DbEntityRegisterProvider, and DbEntityMapperProvider classes

--- lib/src/entities/db_entity_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:sqflite_entity_mapper_orm/src/entities/db_entity.dart';
-import 'package:sqflite_entity_mapper_orm/src/entities/db_entity_builder.dart';
-
-abstract class DbEntityProvider<T> {
-  DbEntityBuilder<T> builder = DbEntityBuilder<T>();
-
-  DbEntity get entity;
-}
