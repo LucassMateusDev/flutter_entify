3d52a6b339d4872a1111856a5d3f55e1923ba5f0
LucassMateus <spechtlucasmateus@gmail.com>
2025-03-13 13:49:35 -0300

    refactor: update imports

--- lib/src/entities/db_entity_register.dart
+++ lib/src/entities/db_entity_register.dart
@@ -1,4 +1,5 @@
-import 'package:sqflite_entity_mapper_orm/sqflite_entity_mapper_orm.dart';
+import 'package:sqflite_entity_mapper_orm/src/entities/db_entity.dart';
+import 'package:sqflite_entity_mapper_orm/src/entities/db_entity_service.dart';
 
 class DbEntityRegister<T> {
   final DbEntity<T> entity;
