8b5b2cb41b704bb38c77a955bdf09206a4887189
LucassMateus <spechtlucasmateus@gmail.com>
2025-05-27 18:30:03 -0300

    feat: implement entity configuration in AppDbContext and add migration for Roles and UserRoles tables from example

--- example/lib/src/data/database/app_db_context.dart
+++ example/lib/src/data/database/app_db_context.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:entify/entify.dart';
-import 'package:example/src/data/database/db_entities.dart';
 import 'package:example/src/domain/entitites.dart';
 
 class AppDbContext extends DbContext with DbTransactionOperations {
@@ -24,10 +23,80 @@ class AppDbContext extends DbContext with DbTransactionOperations {
         .databaseName('example2.db')
         .version(4)
         .withAutoMigrations()
-        .entities(DbEntitites.get())
         // Se você precisa executar algum bind antes de inicializar os dbSets
         .executeBindsBeforeInitialize();
     // .migrations([MigrationV1()]);
     super.onConfiguring(optionsBuilder);
   }
+
+  @override
+  List<DbEntity> configureEntites(DbEntityBuilderProvider provider) => [
+        provider
+            .getSimpleEntityBuilder<Role>()
+            .entityDefinition({
+              'id': int,
+              'name': String,
+              'description': String,
+            })
+            .mapToEntity((map) => Role(
+                  id: map['id'] as int?,
+                  name: map['name'] as String,
+                  description: map['description'] ?? '',
+                ))
+            .toUpdateOrInsert((e) => {
+                  'name': e.name,
+                  'description': e.description,
+                })
+            .build(),
+        provider
+            .getDefaultDbEntityBuilder<User>()
+            .primaryKey((e) => {'id': e.id})
+            .mapToEntity((map) => User(
+                  id: map['id'] as int?,
+                  name: map['name'] as String,
+                  email: map['email'] ?? '',
+                  roles: [],
+                ))
+            .toUpdateOrInsert((e) => {'name': e.name, 'email': e.email})
+            .columns(
+          [
+            IntColumn(
+              name: 'id',
+              isPrimaryKey: true,
+              isNullable: false,
+              isAutoIncrement: true,
+            ),
+            TextColumn(name: 'name', isNullable: false),
+            TextColumn(name: 'email'),
+          ],
+        ).build(),
+        provider
+            .getDefaultDbEntityBuilder<UserRoles>()
+            .primaryKey((e) => {'id': e.id})
+            .uniqueKeys(
+              (e) => [
+                {'idRole': e.idRole, 'idUser': e.idUser},
+              ],
+            )
+            .mapToEntity(
+              (map) => UserRoles(
+                id: map['id'] as int?,
+                idRole: map['idRole'] as int,
+                idUser: map['idUser'] as int,
+              ),
+            )
+            .toUpdateOrInsert((e) => {'idRole': e.idRole, 'idUser': e.idUser})
+            .columns(
+          [
+            IntColumn(
+              name: 'id',
+              isPrimaryKey: true,
+              isNullable: false,
+              isAutoIncrement: true,
+            ),
+            IntColumn(name: 'idRole', isNullable: false),
+            IntColumn(name: 'idUser', isNullable: false),
+          ],
+        ).build(),
+      ];
 }
--- example/lib/src/data/database/migration_v1.dart
+++ example/lib/src/data/database/migration_v1.dart
@@ -1,33 +1,13 @@
 import 'package:entify/entify.dart';
-import 'package:sqflite_common/sqlite_api.dart';
 
-class MigrationV1 implements IMigration {
+class MigrationV1 extends CreateMigration {
   @override
-  void create(Batch batch) {
-    batch.execute('''
-      CREATE TABLE Roles (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL
-      );
-    ''');
-    batch.execute('''
+  void execute(BatchSchemaExecutor executor) {
+    executor.execute('''
       CREATE TABLE Users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL
       );
     ''');
-    batch.execute('''
-      CREATE TABLE UserRoles (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        userId INTEGER NOT NULL,
-        roleId INTEGER NOT NULL,
-        UNIQUE (userId, roleId),
-        FOREIGN KEY (userId) REFERENCES Users(id),
-        FOREIGN KEY (roleId) REFERENCES Roles(id)
-      );
-    ''');
   }
-
-  @override
-  void update(Batch batch) {}
 }
